871d87b6d0ed259f4abae00268c6aa9349acf3ff
ANJALI GANVIT <160722894+Ganvitanjali@users.noreply.github.com>
2025-04-15 14:06:22 +0530

    InquiryRoutes.js

--- Project1/src/routes/InquiryRoutes.js
+++ Project1/src/routes/InquiryRoutes.js
@@ -4,4 +4,10 @@ const inquiryController = require("../controllers/InquiryController");
 routes.post("/add",inquiryController.addInquiry);
 routes.get("/get",inquiryController.getInquiry);
 routes.post("/reply/:id", inquiryController.replyInquiry);
+
+// Reply to a specific inquiry by ID
+routes.put("/inquiries/reply/:id", inquiryController.replyInquiry);
+
+routes.get("/unreadreplies/:userId", inquiryController.getUnreadReplies);
+
 module.exports = routes;
